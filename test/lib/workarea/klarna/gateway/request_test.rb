f43e75b0cd84404c9286735d32503eb0f6efa5cd
Matt Duffy <mttdffy@gmail.com>
2020-09-21 14:17:00 -0400

    Clear out klarna env vars before running test
    No changelog

--- test/lib/workarea/klarna/gateway/request_test.rb
+++ test/lib/workarea/klarna/gateway/request_test.rb
@@ -4,7 +4,27 @@ module Workarea
   module Klarna
     class Gateway
       class RequestTest < TestCase
-        setup :configure_klarna_data, :payment
+        setup :clear_env, :configure_klarna_data, :payment
+        teardown :reset_env
+
+        def clear_env
+          @env_na_username = ENV['WORKAREA_KLARNA_NA_USERNAME']
+          @env_na_password = ENV['WORKAREA_KLARNA_NA_PASSWORD']
+          @env_eur_username = ENV['WORKAREA_KLARNA_EUR_USERNAME']
+          @env_eur_password = ENV['WORKAREA_KLARNA_EUR_PASSWORD']
+
+          ENV.delete('WORKAREA_KLARNA_NA_USERNAME')
+          ENV.delete('WORKAREA_KLARNA_NA_PASSWORD')
+          ENV.delete('WORKAREA_KLARNA_EUR_USERNAME')
+          ENV.delete('WORKAREA_KLARNA_EUR_PASSWORD')
+        end
+
+        def reset_env
+          ENV['WORKAREA_KLARNA_NA_USERNAME'] = @env_na_username
+          ENV['WORKAREA_KLARNA_NA_PASSWORD'] = @env_na_password
+          ENV['WORKAREA_KLARNA_EUR_USERNAME'] = @env_eur_username
+          ENV['WORKAREA_KLARNA_EUR_PASSWORD'] = @env_eur_password
+        end
 
         def order
           @order ||= create_order
@@ -43,11 +63,8 @@ module Workarea
           payment.set_address(supported_eur_address)
           assert_equal(Workarea.config.klarna_eur_username, request.username)
 
-          @env_eur_user = ENV['WORKAREA_KLARNA_EUR_USERNAME']
           ENV['WORKAREA_KLARNA_EUR_USERNAME'] = 'env_eur_user'
           assert_equal('env_eur_user', request.username)
-        ensure
-          ENV['WORKAREA_KLARNA_EUR_USERNAME'] = @env_eur_user
         end
 
         def test_password
@@ -62,11 +79,8 @@ module Workarea
           payment.set_address(supported_eur_address)
           assert_equal(Workarea.config.klarna_eur_password, request.password)
 
-          @env_eur_pass = ENV['WORKAREA_KLARNA_EUR_PASSWORD']
           ENV['WORKAREA_KLARNA_EUR_PASSWORD'] = 'env_eur_user'
           assert_equal('env_eur_user', request.password)
-        ensure
-          ENV['WORKAREA_KLARNA_EUR_PASSWORD'] = @env_eur_pass
         end
 
         def test_continent_key
